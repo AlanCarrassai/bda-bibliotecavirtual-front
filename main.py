aa23d6984cd0f1f1f9779d3fbf30adc2a24fd970
Eduarda <geraldoeduarda14@gmail.com>
2026-06-25 11:05:50 -0300

    Add main.py to initiate performance check

---
+++ main.py
@@ -0,0 +1,10 @@
+import geracaodedados
+import indices
+import buscas
+
+if __name__ == "__main__":
+    print("Iniciando verificação de desempenho")
+
+    buscas.executar()
+    
+    print("\nDesempenho verificado")
